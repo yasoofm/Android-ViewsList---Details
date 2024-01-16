e17f2807cc922b1b0877efa4ad092cc785bee008
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-16 15:20:12 +0300

    Transaction model

---
+++ app/src/main/java/com/example/androidproject/models/Transaction.java
@@ -0,0 +1,36 @@
+package com.example.androidproject.models;
+
+public class Transaction {
+    private int id, account;
+    private String date;
+    //private Enum<String> type;
+    private double amount, balance;
+
+    Transaction(int id, int account, String date, double balance, double amount){
+        this.id = id;
+        this.account = account;
+        this.date = date;
+        this.balance = balance;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
