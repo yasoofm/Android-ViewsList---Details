6295d7b8bfcd7ff6778c4336a2f3af8fe0f9314c
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-16 15:41:47 +0300

    add history repo

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView historyRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/example/androidproject/repositories/historyRepo.java
@@ -0,0 +1,30 @@
+package com.example.androidproject.repositories;
+
+import com.example.androidproject.models.Transaction;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+public class historyRepo {
+    private static historyRepo instance;
+
+    private historyRepo(){
+
+    }
+
+    public static historyRepo getInstance(){
+        if(instance == null){
+            instance = new historyRepo();
+        }
+        return instance;
+    }
+
+    public ArrayList<Transaction> dummyTransactions(int numberOfItems){
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        for(int i = 0; i < numberOfItems; i++){
+            Transaction transaction = new Transaction(i + 1, i + 1, String.valueOf(Calendar.getInstance().getTime()), 500.5, 200.5);
+        }
+        return transactions;
+    }
+}
